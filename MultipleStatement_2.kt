bc598645aa572dbc6610728a0ce741c5b7805ffa
Mohamed <Mohamedmezziane19@gmail.com>
2024-10-14 16:22:28 +0100

    Tutto

---
+++ MultipleStatement_2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val talaja = "Samak"
+
+    when(talaja) {
+        "Khodar", "Btata", "Maticha", "Basla" -> println("Tayeb Tajin Bel Khodar")
+        "Samak" -> println("Tayeb Tajin Del Samak")
+        "Fawakih" -> println("Dir Hasir Del Fawakih")
+
+        else -> println("Siir Siir Siir Lsoq Tsakhar")
+    }
+}
